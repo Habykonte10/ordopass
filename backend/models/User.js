3094c6cbd4307918087332bed323e34479993f17
Habykonte10 <h.konte4@isepdiamniadio.edu.sn>
2026-01-25 14:58:28 +0100

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, unique: true },
-  password: String,
-  role: String
-});
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ["medecin", "pharmacie", "admin"],
+    required: true
+  }
+}, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
