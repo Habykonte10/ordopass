50dd25bfe9a3019b0a1f1f19ed8cb49f41efa6f0
Habykonte10 <h.konte4@isepdiamniadio.edu.sn>
2026-02-10 16:51:04 +0100

    Add files via upload

--- User.js
+++ User.js
@@ -1,18 +1,10 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, required: true }
-});
-
-module.exports = mongoose.model("User", userSchema);
-const mongoose = require("mongoose");
-
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
 
   password: {
